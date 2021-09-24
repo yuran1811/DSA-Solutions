3fcfbf57f81ae8e87897007a1acba9cf7239ff91
Yuran <76398420+yuran1811@users.noreply.github.com>
2021-09-24 13:51:44 +0700

    Create Dinic.cpp

---
+++ Algorithms/Flow/Dinic.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1080, oo = 1e9 + 7;
+
+int d[N], c[N][N], f[N][N];
+int dfs[N], cnt = 0;
+vector <int> a[N];
+int n, m, s, t;
+
+bool BFS(int s, int t) 
+{
+	memset(d, 0, sizeof d); d[s] = 1;
+	queue <int> q; q.push(s);
+
+	while (q.size()) 
+	{
+		int u = q.front(); q.pop();
+		if (u == t) return true;
+		
+		for (int v : a[u])
+			if (!d[v] && f[u][v] < c[u][v])
+				d[v] = d[u] + 1, q.push(v);
+	}
+	return false;
+}
+
+int DFS(int u, int t, int minn) 
+{
+	if (u == t) return minn;
+	if (dfs[u] != cnt) dfs[u] = cnt; else return 0;
+
+	for (int v : a[u])
+		if (f[u][v] < c[u][v])
+			if (dfs[v] != cnt && d[v] == d[u] + 1)
+				if (int x = DFS(v, t, min(minn, c[u][v] - f[u][v]))) 
+					return f[u][v] += x, f[v][u] -= x, x;
+	return 0;
+}
+
+int main() 
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	cin >> n >> m >> s >> t;
+	for (int i = 1, u, v, w; i <= m; i++) 
+		cin >> u >> v >> w,
+		a[u].push_back(v),
+		a[v].push_back(u),
+		c[u][v] += w;
+
+	int maxflow = 0;
+	while (BFS(s, t))
+		while (int x = (cnt++, DFS(s, t, oo)))
+			maxflow += x;
+	cout << maxflow;
+	return 0;
+}
