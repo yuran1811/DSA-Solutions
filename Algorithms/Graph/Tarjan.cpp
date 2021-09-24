143a78b882d5a1df49565a8e677bc077767687bc
Yuran <76398420+yuran1811@users.noreply.github.com>
2021-09-24 17:29:35 +0700

    Create Tarjan.cpp

---
+++ Algorithms/Graph/Tarjan.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 5, oo = 1e9 + 7;
+
+vector <int> Node[N], a[N], s;
+int Num[N], Low[N], cnt = 0;
+int n, m, NumCpn;
+
+int mnz(int &a, int b) {return a > b ? a = b, 1 : 0;}
+
+void Tarjan(int u)
+{
+	Num[u] = Low[u] = ++cnt;
+	s.push_back(u);
+
+	for (int v : a[u])
+		if (!Num[v])
+		{
+			Tarjan(v);
+			mnz(Low[u], Low[v]);
+		}
+		else mnz(Low[u], Num[v]);
+
+	if (Low[u] == Num[u])
+	{
+		NumCpn++;
+		int v;
+		do
+		{
+			v = s.back(); s.pop_back();
+			Node[NumCpn].push_back(v);
+			Num[v] = Low[v] = oo;
+		} while (v != u);
+		sort(Node[NumCpn].begin(), Node[NumCpn].end());
+	}
+}
+
+void Load_Data()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	cin >> n >> m;
+	for (int i = 1, u, v; i <= m; i++)
+		cin >> u >> v, a[u].push_back(v);
+
+	for (int u = 1; u <= n; u++)
+		if (!Num[u]) Tarjan(u);
+}
+
+int Solve()
+{
+	Load_Data();
+	cout << NumCpn << '\n';
+	for (int i = 1; i <= NumCpn; i++, cout << '\n')
+		for (int v : Node[i])
+			cout << v << " ";
+	return 0;
+}
+
+int main() {Solve();}
