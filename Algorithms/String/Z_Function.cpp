fa5cffe9e66feb028e15dc11c77c4e05effea887
Yuran <76398420+yuran1811@users.noreply.github.com>
2021-09-25 12:08:05 +0700

    Create Z_Function.cpp

---
+++ Algorithms/String/Z_Function.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int N = 1e6 + 5;
+
+vector <int> Z;
+string s, t;
+
+void Z_Build(const string &s)
+{
+	int n = s.size(), l, r;
+	Z.resize(n);
+	l = r = 0;
+	for (int i = 1; i < n; i++)
+	{
+		if (i <= r)
+			Z[i] = min(r - i + 1, Z[i - l]);
+		while (i + Z[i] < n && s[i + Z[i]] == s[Z[i]]) Z[i]++;
+		if (r < i + Z[i] - 1)
+			l = i, r = i + Z[i] - 1;
+	}
+}
+
+void Load_Data()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	cin >> s >> t;
+	s = t + '.' + s;
+	Z_Build(s);
+	for (int i = t.size() + 1; i < int (s.size()); i++)
+		if (int (t.size()) <= Z[i])
+			cout << i - t.size() << " ";
+}
+
+int Scorpio()
+{
+	Load_Data();
+	return 0;
+}
+
+int main() {Scorpio();}
