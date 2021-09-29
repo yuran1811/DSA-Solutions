6cc7f94f1a1c4438e8ab5a10e81af97a07e68a3c
Yuran <76398420+yuran1811@users.noreply.github.com>
2021-09-29 19:00:55 +0700

    Update Binary-Indexed-Tree.cpp

--- Algorithms/Data Structures/Binary-Indexed-Tree.cpp
+++ Algorithms/Data Structures/Binary-Indexed-Tree.cpp
@@ -1,12 +1,38 @@
 struct BIT
 {
-	vector <int> bit;
-	void Init(int n) {bit.resize(n + 5, 0);}
-	void Up(int i, int val) {for (; i <= n; i += -i & i) bit[i] += val;}
+	vector <int> bit1, bit2;
+
+	void Init(int n)
+	{
+		bit1.resize(n + 5, 0);
+		bit2.resize(n + 5, 0);
+	}
+
+	void Up(vector <int> &bit, int i, int val)
+	{
+		for (; i <= n; i += -i & i)
+			bit[i] += val;
+	}
+
+	void Range_Up(int l, int r, int val)
+	{
+		Up(bit1, l, val);
+		Up(bit1, r + 1, -val);
+		Up(bit2, l, val * (l - 1));
+		Up(bit2, r + 1, -val * r);
+	}
+
 	int Get(int i, int res = 0)
 	{
 		for (; i > 0; i -= -i & i)
 			res += bit[i];
 		return res;
+	}
+
+	int Range_Get(int l, int r)
+	{
+		int SumR = Get(bit1, r) * r - Get(bit2, r);
+		int SumL = Get(bit1, l - 1) * (l - 1) - Get(bit2, l - 1);
+		return SumR - SumL;
 	}
 };
