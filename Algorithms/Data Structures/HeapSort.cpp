b435896d6778751a55fdb5f06092a6b38c80a8ba
Yuran <76398420+yuran1811@users.noreply.github.com>
2021-09-29 19:05:34 +0700

    Create HeapSort.cpp

---
+++ Algorithms/Data Structures/HeapSort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+const int N = 1e5 + 5;
+
+int n, a[N];
+
+void HeapSort(int n, int i)
+{
+	int root = i;
+	int l = 2 * i + 1;
+	int r = 2 * i + 2;
+	if (l < n && a[l] > a[root]) root = l;
+	if (r < n && a[r] > a[root]) root = r;
+	if (root != i)
+	{
+		swap(a[i], a[root]);
+		HeapSort(n, root);
+	}
+} 
+
+int main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	cin >> n; for (int i = 0; i < n; i++) cin >> a[i];
+	for (int i = n - 1; i >= 0; i--) HeapSort(n, i);
+	for (int i = n - 1; i >= 0; i--)
+		swap(a[0], a[i]), HeapSort(i, 0);
+	for (int i = 0; i < n; i++)
+		cout << a[i] << " ";
+	return 0;
+}
