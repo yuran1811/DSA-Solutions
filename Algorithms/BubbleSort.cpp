b6be31b7310ff1f7968a5125f893b03f5cb0e3eb
Yuran <76398420+yuran1811@users.noreply.github.com>
2021-10-01 21:20:28 +0700

    Create BubbleSort.cpp

---
+++ Algorithms/BubbleSort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+const int N = 1e5 + 5;
+
+int n, a[N];
+
+void SORT()
+{
+	bool isSwap;
+	for (int i = 1; i < n; i++)
+	{
+		isSwap = 0;
+		for (int j = 1; j <= n - i; j++)
+			if (a[j] > a[j + 1])
+				swap(a[j], a[j + 1]), isSwap = 1;
+		if (!isSwap) break;
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+		cin >> a[i];
+
+	SORT();
+	for (int i = 1; i <= n; i++)
+		cout << a[i] << " ";
+	return 0;
+}
