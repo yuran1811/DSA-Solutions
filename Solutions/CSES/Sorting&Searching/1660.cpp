15bc56ba4e4e759d21b278db35acf2fce202c526
Yuran <76398420+yuran1811@users.noreply.github.com>
2021-10-25 15:21:11 +0700

    Create 1660.cpp

---
+++ Solutions/CSES/Sorting&Searching/1660.cpp
@@ -0,0 +1,23 @@
+// https://cses.fi/problemset/task/1660/
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 2e5 + 5;
+ 
+int n, k;
+map <long long, long long> f;
+long long res = 0, sum = 0;
+ 
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> n >> k; 
+    for (int i = 1, x; i <= n; i++)
+    {
+        cin >> x;
+        f[sum]++;
+        sum += x;
+        res += f[sum - k];
+    }
+    cout << res;
+    return 0;
+}
