270e246f74ef42384cf826f2ec4b8d553a331bb9
Yuran <76398420+yuran1811@users.noreply.github.com>
2021-10-29 12:57:57 +0700

    Update 1638.cpp

--- Solutions/CSES/Dynamic Programming/1638.cpp
+++ Solutions/CSES/Dynamic Programming/1638.cpp
@@ -9,25 +9,26 @@ char c[N][N];
 
 int main()
 {
-    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     cin >> n;
-    for(int i=1;i<=n;i++)
-      for(int j=1;j<=n;j++)
+    for (int i = 1; i <= n; i++)
+      for (int j = 1; j <= n; j++)
           cin >> c[i][j];
   
-    if (c[1][1]!='.' || c[n][n]!='.') return cout << 0, 0;
+    if (c[1][1] ^ '.' || c[n][n] ^ '.') return cout << 0, 0;
   
-    f[1][1]=1;
-    for(int i=1;i<=n;i++)
-    for(int j=1;j<=n;j++)
-        if (c[i][j]=='.')
+    f[1][1] = 1;
+    for (int i = 1; i <= n; i++)
+    for (int j = 1; j <= n; j++)
+        if (c[i][j] == '.')
         {
-            if (c[i-1][j]=='.' && c[i][j-1]=='.')
-                f[i][j]+=(f[i-1][j]%oo+f[i][j-1]%oo)%oo; else
-            if (c[i-1][j]=='.' && c[i][j-1]!='.')
-                f[i][j]+=f[i-1][j]%oo; else
-            if (c[i-1][j]!='.' && c[i][j-1]=='.')
-                f[i][j]+=f[i][j-1]%oo;
+            if (c[i - 1][j] == '.')
+            {
+                if (c[i][j - 1] == '.')
+                    f[i][j] += (f[i - 1][j] % oo + f[i][j - 1] % oo) % oo; else
+                    f[i][j] += f[i - 1][j] % oo;
+            } else if (c[i-1][j] != '.' && c[i][j-1] == '.')
+                f[i][j] += f[i][j - 1] % oo;
         }
     cout << f[n][n];
     return 0;
