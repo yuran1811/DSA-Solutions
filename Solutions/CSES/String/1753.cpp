4ade799b25e36247acc968fa74104bc621fa00d0
Yuran <76398420+yuran1811@users.noreply.github.com>
2023-06-03 08:35:09 +0700

    Update 1753.cpp

--- Solutions/CSES/String/1753.cpp
+++ Solutions/CSES/String/1753.cpp
@@ -1,46 +1,56 @@
-#include <bits/stdc++.h>
+#include <iostream>
+ 
 using namespace std;
  
 typedef long long ll;
-
-const int N = 2e6 + 5;
-const ll BASE = 311, MOD = 1e9 + 7;
  
-int n, m, cnt = 0;
-string s, t;
+const int N = 1e6 + 5;
+const int BASE = 311;
+const int MOD = 1e9 + 1993;
+ 
+int n, m;
 ll Pow[N];
  
-struct HASH
+struct HASH {
+    ll table[N];
+ 
+    void init(string s, int &n) {
+        n = s.size();
+        s = " " + s;
+        for (int i = 1; i <= n; i++)
+            table[i] = (1LL * table[i - 1] * BASE + s[i] - 'a' + 1) % MOD;
+    }
+ 
+    ll get(int i, int j) {
+        return (table[j] - 1LL * table[i - 1] * Pow[j - i + 1] + 1LL * MOD * MOD) % MOD;
+    }
+} hashOrigin, hashPattern;
+ 
+void factorialBuild()
 {
-	ll Hash[N];
- 
-	void in(string s, int &n)
-	{
-		n = s.size();
-		s = " " + s;
-		for (int i = 1; i <= n; i++)
-			Hash[i] = (Hash[i - 1] * BASE + s[i] - 'a' + 1) % MOD;
-	}
- 
-	ll get(int i, int j) {return (Hash[j] - Hash[i - 1] * Pow[j - i + 1] + MOD * MOD) % MOD;}
-} H1, H2;
- 
-void build()
-{
-	Pow[0] = 1;
-	for (int i = 1; i < N; i++)
-		Pow[i] = Pow[i - 1] * BASE % MOD;
+    Pow[0] = 1;
+    for (int i = 1; i < N; i++)
+        Pow[i] = 1LL * Pow[i - 1] * BASE % MOD;
 }
  
 int main()
 {
-	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	cin >> s >> t;
-	build();
-	H1.in(s, n); H2.in(t, m);
-	for (int i = 1; i <= n - m + 1; i++)
-		if (H2.Hash[m] == H1.get(i, i + m - 1))
-			cnt++;
-	cout << cnt;
-	return 0;
+    ios_base::sync_with_stdio(0); cin.tie(0);
+ 
+    factorialBuild();
+ 
+    string s, t;
+    cin >> s >> t;
+ 
+    hashOrigin.init(s, n);
+    hashPattern.init(t, m);
+ 
+    int cnt = 0;
+    for (int i = 1; i <= n - m + 1; i++)
+        if (hashPattern.table[m] == hashOrigin.get(i, i + m - 1))
+            cnt++;
+ 
+    cout << cnt;
+ 
+    return 0;
 }
