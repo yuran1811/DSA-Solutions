32b58c05758ea23cff404307bb009c5673510dcf
Yuran <trieuvanbd123@gmail.com>
2024-08-19 08:42:12 +0700

    Update CountingSort.cpp

--- DSACollection/Sorting/CountingSort.cpp
+++ DSACollection/Sorting/CountingSort.cpp
@@ -6,41 +6,33 @@ using namespace std;
 const int MAX_NUM = 1e6 + 7;
 
 vector<int> countingSort(const vector<int> &a) {
-  int n = a.size();
-  vector<int> b(n), c(MAX_NUM, 0);
+  const int n = a.size();
+  vector<int> b(n), d(MAX_NUM, 0);
 
-  for (const int &_ : a) c[_]++;
-
-  for (int i = 1; i < MAX_NUM; i++) {
-    c[i] += c[i - 1];
-  }
-
-  for (int i = n - 1; i >= 0; i--) {
-    b[--c[a[i]]] = a[i];
-  }
+  int maxNum = a[0];
+  for (const int &_ : a) d[_]++, maxNum = max(maxNum, _);
+  for (int i = 1; i <= maxNum; i++) d[i] += d[i - 1];
+  
+  for (int i = n - 1; i >= 0; i--) b[--d[a[i]]] = a[i];
 
   return b;
 }
 
 vector<int> radixSort(const vector<int> &a) {
+  const int n = a.size();
   vector<int> b = a;
 
-  for (int exp = 1; MAX_NUM / exp > 0; exp *= 10) {
-    vector<int> c(MAX_NUM, 0);
+  int maxNum = a[0];
+  for (const int &_ : a) maxNum = max(maxNum, _);
 
-    for (const int &_ : b) c[_]++;
+  for (int exp = 1; maxNum / exp > 0; exp *= 10) {
+    vector<int> d(10, 0), c(n);
 
-    for (int i = 1; i < MAX_NUM; i++) {
-      c[i] += c[i - 1];
-    }
+    for (const int &_ : b) d[_ / exp % 10]++;
+    for (int i = 1; i <= 9; i++) d[i] += d[i - 1];
 
-    vector<int> d(b.size());
-
-    for (int i = b.size() - 1; i >= 0; i--) {
-      d[--c[b[i] / exp % 10]] = b[i];
-    }
-
-    b = d;
+    for (int i = n - 1; i >= 0; i--) c[--d[b[i] / exp % 10]] = b[i];
+    b = c;
   }
 
   return b;
